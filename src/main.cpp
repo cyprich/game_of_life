f5ff4e0931e3ed9c2442e5c565aec74df35bc198
cyprich <cypooriginal@gmail.com>
2024-11-20 16:54:43 +0100

    empty raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "raylib.h"
 
 int main (int argc, char *argv[]) {
 	std::cout << "Conway's Game of Life" << std::endl;
+	InitWindow(800, 800, "Conway's Game of Life");
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
 	return 0;
 }
